4c0a8141b954371d9b3edcad85fc39b7a176b181
Eric Edwards <eric@gam3r.local>
2018-10-29 21:09:49 -0700

    break things.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <AppBar Postion="static"><ToolBar></ToolBar>Test</AppBar>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Type <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
